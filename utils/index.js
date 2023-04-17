c72c54b227d6193c1a75a56781d41658df73dbdf
Linda Paz <lindacsndelapaz@gmail.com>
2023-04-17 18:17:36 -0300

    first commit

---
+++ utils/index.js
@@ -0,0 +1,13 @@
+export const suma = (a, b ) => a + b;
+
+export const resta = (a, b ) => a - b;
+
+export const promedio = (a, b ) => (a + b ) / 2;
+
+export const palindromo = (str) => {
+    const strReverse = str.split("").reverse().join("");
+    return str = strReverse;
+}
+
+
+
