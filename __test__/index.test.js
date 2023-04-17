d84e6b19f6d043b26261f3bd183fd7c00cedea3e
Linda Paz <lindacsndelapaz@gmail.com>
2023-04-17 20:40:57 -0300

    add test with 4 variables

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -2,6 +2,11 @@ const {suma} = require('../utils');
 
 describe('Archivo de utils', () => {
     test('Sumo dos numeros correctamente', () => {
-        console.log(suma(1,2));
+        // console.log(suma(1,2));
+        expect(suma(1,2)).toBe(3);
+        expect(suma(2,2)).toBe(4);
+        expect(suma(1,4)).toBe(5);
+        expect(suma(1,5)).toBe(6);
+
     });
 });
